62385e31c5ae8b6fb00ed148ff2252c113e297d7
Biagiarchiefz <biagiiarchie@gmail.com>
2024-04-18 16:58:00 +0700

    tugas5

--- tugas/tugas5/5a.php
+++ tugas/tugas5/5a.php
@@ -5,70 +5,70 @@ $mahasiswa = [
     "npm" => "233040006",
     "jurusan" => "Teknik Informatika",
     "email" => "biagi@gmail.com",
-    "gambar" => "ang1.png"
+    "gambar" => "ape1.jpg"
     ],
     [
         "nama" => "Madara",
         "npm" => "233040017",
         "jurusan" => "Teknik Informatika",
         "email" => "ArahArah@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "gaa1.jpg"
     ],
     [
         "nama" => "Saske",
         "npm" => "233040018",
         "jurusan" => "Teknik Informatika",
         "email" => "Saske@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "gara1.jpg"
     ],
     [
         "nama" => "Narto",
         "npm" => "233040019",
         "jurusan" => "Teknik Informatika",
         "email" => "Narto@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "hina1.jpg"
     ],
     [
         "nama" => "Miranda",
         "npm" => "233040020",
         "jurusan" => "Teknik Informatika",
         "email" => "Miranda@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "ita1.jpg"
     ],
     [
         "nama" => "Murtad",
         "npm" => "233040021",
         "jurusan" => "Teknik Informatika",
         "email" => "Murtad@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "kakasi1.jpg"
     ],
     [
         "nama" => "Madun",
         "npm" => "233040022",
-        "jurusan" => "Teknik Informatika",
+        "jurusan" => "Teknik Mesin",
         "email" => "Madun@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "mina1.jpg"
     ],
     [
         "nama" => "Murod",
         "npm" => "233040023",
         "jurusan" => "Teknik Informatika",
         "email" => "Mrod@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "naru1.png"
     ],
     [
         "nama" => "Ibrahim",
         "npm" => "233040024",
-        "jurusan" => "Teknik Informatika",
+        "jurusan" => "Teknik industri",
         "email" => "Ahimh@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "obi1.png"
     ],
     [
         "nama" => "Zaki",
         "npm" => "233040025",
-        "jurusan" => "Teknik Informatika",
+        "jurusan" => "Teknik Mesin",
         "email" => "Zak@gmail.com",
-        "gambar" => "ang2.png"
+        "gambar" => "sai1.jpg"
     ]
     
 
